341f9239200904779063ec0141c5c811de8374f2
Nirzar <nirzarsnowbell@gmail.com>
2021-04-07 17:21:23 -0400

    into to react-hook-form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,205 +1,51 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
-function Form() {
-    return (
-        <div className="Form mb-50">
-            <section className="contest-info page-title">
-            <section className="grid-container text-center">
-                <h1>The prizes and chances of winning</h1>
-                <section className="row column">
-                    <p>Every customer of our product got a chance of winning a free dairy package!</p>
-                    <p><strong>Yogurt tips:</strong> winners will be determined by random draw but may have their entries removed for any reason by the judges.</p>
-                    <p>Users under the age of 16 are not eligible for the activity without a guardian's permission.</p>
-                </section>
-            </section>
-        </section>
-        <section className="grid-container">
-            <section className="grid-x grid-padding-x">
-                <section className="cell large-6 skill-test">
-                    <legend className="skill-title">Skill Test</legend>
-                    <form className="grid-y" action="#">
-                        <label for="skillTest" id="skillTest">import_question_here</label>
-                        <section className="grid-x">
-                            <section className="cell large-3"></section>
-                            <section className="cell large-6">
-                                <input className="input-box" type="text" name="skillTestInput" id="skillTestInput" placeholder="Answer of the question" />
-                            </section>
-                            <section className="cell large-3"></section>
-                        </section>
-                        
-                        <p className="error-msg" id="skillTestError">error_here</p>
-                    </form>
-                </section>
-                <section className="cell large-12">
-                    <form className="grid-x" action="#">
-                        <section className="cell large-8">
-                            <fieldset>
-                                <legend>Personal Information</legend>
-                                <section className="grid-x grid-padding-x">
-                                    <section className="cell large-2">
-                                        <label for="salutation">Prefix
-                                            <select name="salutation" id="salutation">
-                                                <option value="Mr">Mr</option>
-                                                <option value="Mrs">Mrs</option>
-                                                <option value="Miss">Miss</option>
-                                            </select>
-                                        </label>
-                                        <p className="error-msg" id="salutationError">error_here</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="firstName">First Name
-                                            <input type="text" className="input-box" name="firstName" id="firstName"
-                                                placeholder="Enter your First name" />
-                                        </label>
-                                        <p className="error-msg" id="firstNameError">error_here</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="lastName">Last Name
-                                            <input type="text" className="input-box" name="lastName" id="lastName"
-                                                placeholder="Enter your Last name" />
-                                        </label>
-                                        <p className="error-msg" id="lastNameError">error_here</p>
-                                    </section>
-                                    <section className="cell large-8">
-                                        <label for="dateOfBirth">Date of Birth
-                                            <input type="date" className="input-box" name="dateOfBirth" id="dateOfBirth"
-                                                placeholder="YYYY-MM-DD" />
-                                        </label>
-                                        <p className="error-msg" id="dateOfBirthError">error_here</p>
-                                    </section>
-                                </section>
-                            </fieldset>
-                        </section>
-                        <section className="cell large-8" id="guardianDetailsSection">
-                            <fieldset>
-                                <legend>Guardian's Information</legend>
-                                <p className="error-msg">Please fill guardian details, you are under 16.</p>
-                                <section className="grid-x grid-padding-x">
-                                    <section className="cell large-2">
-                                        <label for="guardianSalutation">Prefix
-                                            <select name="guardianSalutation" id="guardianSalutation">
-                                                <option value="Mr">Mr</option>
-                                                <option value="Mrs">Mrs</option>
-                                                <option value="Miss">Miss</option>
-                                            </select>
-                                        </label>
-                                        <p className="error-msg" id="guardianSalutationError">error_here</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="guardianLastName">Last Name
-                                            <input type="text" className="input-box" name="guardianLastName"
-                                                id="guardianLastName" placeholder="Enter Guardian's Last name" />
-                                        </label>
-                                        <p className="error-msg" id="guardianLastNameError">error_here</p>
-                                    </section>
-                                    <section className="cell large-6">
-                                        <label for="guardianEmailId">Email
-                                            <input type="text" className="input-box" name="guardianEmailId"
-                                                id="guardianEmailId" placeholder="Enter Guardian's email" />
-                                        </label>
-                                        <p className="error-msg" id="guardianEmailIdError">error_here</p>
-                                    </section>
-                                    <section className="cell large-6">
-                                        <label for="guardianPhoneNum">Phone number
-                                            <input type="text" className="input-box" name="guardianPhoneNum"
-                                                id="guardianPhoneNum" placeholder="Enter guardian's phone number" />
-                                        </label>
-                                        <p className="error-msg" id="guardianPhoneNumError">error_here</p>
-                                    </section>
-                                </section>
-                            </fieldset>
-                        </section>
-                        <section className="cell large-8">
-                            <fieldset>
-                                <legend>Contact Information</legend>
-                                <section className="grid-x grid-padding-x">
-                                    <section className="cell large-6">
-                                        <label for="emailId">Email
-                                            <input type="text" className="input-box" name="emailId" id="emailId"
-                                                placeholder="Enter email" />
-                                        </label>
-                                        <p className="error-msg" id="emailIdError">error_here</p>
-                                    </section>
-                                    <section className="cell large-6">
-                                        <label for="phoneNum">Phone number
-                                            <input type="text" className="input-box" name="phoneNum" id="phoneNum"
-                                                placeholder="Enter phone number" />
-                                        </label>
-                                        <p className="error-msg" id="phoneNumError">error_here</p>
-                                    </section>
-                                    <section className="cell large-8">
-                                        <label for="homeAddress1">Address Line 1
-                                            <input type="text" className="input-box" name="homeAddress1" id="homeAddress"
-                                                placeholder="Enter address line 1" />
-                                        </label>
-                                        <p className="error-msg" id="homeAddressError">error_here</p>
-                                    </section>
-                                    <section className="cell large-8">
-                                        <label for="homeAddress2">Address Line 2
-                                            <input type="text" className="input-box" name="homeAddress2" id="homeAddress2"
-                                                placeholder="Enter address line 2" />
-                                        </label>
-                                        <p className="error-msg">&nbsp;</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="city">City
-                                            <input type="text" className="input-box" name="city" id="city"
-                                                placeholder="Enter city name" />
-                                        </label>
-                                        <p className="error-msg" id="cityError">error_here</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="postalCode">ZIP / Postal Code
-                                            <input type="text" className="input-box" name="postalCode" id="postalCode"
-                                                placeholder="Enter city name" />
-                                        </label>
-                                        <p className="error-msg" id="postalCodeError">error_here</p>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="province">Province
-                                            <select className="input-box" name="province" id="province">
-                                                <option value="Alberta">AB</option>
-                                                <option value="British Colombia">BC</option>
-                                                <option value="Manitoba">MB</option>
-                                                <option value="New Brunswick">NB</option>
-                                                <option value="Newfoundland and Labrador">NL</option>
-                                                <option value="Northwest Territories">NT</option>
-                                                <option value="Nova Scotia">NS</option>
-                                                <option value="Nonavut">NU</option>
-                                                <option value="Ontario">ON</option>
-                                                <option value="Prince Edward Island">PE</option>
-                                                <option value="Quebec">QC</option>
-                                                <option value="Saskatchewan">SK</option>
-                                                <option value="Yukon">YT</option>
-                                            </select>
-                                        </label>
-                                    </section>
-                                    <section className="cell large-5">
-                                        <label for="country">Country
-                                            <input type="text" className="input-box" name="country" id="country" disabled="disabled" value="Canada"/>
-                                        </label>
-                                        <p className="error-msg" id="postalCodeError">error_here</p>
-                                    </section>
-                                </section>
-                                <input type="checkbox" id="termsAndConditions" name="termsAndConditions" required/><label for="termsAndConditions"> I accept <a href="term-condition.html">terms and condition</a> for the contest.</label><br/>
-                                <input type="checkbox" id="newsletterSignup" name="termsAndConditions" required/><label for="termsAndConditions"> Sign me up to receive emails! (optional)</label>
-                                <p className="error-msg" id="termsAndConditionsError">error_here</p>
-                            </fieldset>
-                        </section>
-                        <section className="cell large-8">
-                        <section className="grid-x grid-padding-x">
-                            <section className="cell large-12 small-12">
-                                <div className="button" id="formSubmit">Submit</div>
-                                <div className="button secondary" id="formCancel">Cancel</div>
-                            </section>
-                        </section>
-                        </section>
-                    </form>
-                </section>
-            </section>
-        </section>
-        </div>
-    );
+export default function App() {
+  const { register, handleSubmit, errors, reset, formState } = useForm();
+  const { isDirty, isSubmitting } = formState;
+
+  const onSubmit = (data) => alert(JSON.stringify(data, null, 2));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1> Your Data</h1>
+      <div>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Full Name"
+          name="name"
+          ref={register({ required: "Name Required " })}
+        />
+        <span>{errors.name?.message}</span>
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          ref={register({
+            required: "Email Required",
+            pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" },
+          })}
+        />
+        <span>{errors.email?.message}</span>
+      </div>
+      <div>
+        <label>Accept Terms</label>
+        <input
+          type="checkbox"
+          placeholder="Accept Terms"
+          name="acceptedTerms"
+          ref={register({ required: true })}
+        />
+        {errors.acceptedTerms && <span>You must accepet the terms</span>}
+      </div>
+
+      <input type="submit" onClick={reset} disabled={!isDirty || isSubmitting}>
+        Reset
+      <input type="submit" disabled={isSubmitting} />
+    </form>
+  );
 }
-
-export default Form;
