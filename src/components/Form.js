87fdb3a2328458f9e2477f5a3e0a61a5db2a68a0
Nirzar <nirzarsnowbell@gmail.com>
2021-04-08 05:19:48 -0400

    latest updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,87 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import DatePicker from "react-datepicker";
-import { addDays } from 'date-fns';
-import { useHistory } from "react-router-dom";
-import "react-datepicker/dist/react-datepicker.css";
+import React, { Component } from 'react';
 
-export const Form = (children, ...props) => {
-	const { register, handleSubmit } = useForm();
-	const history = useHistory();
+import Input from './Input';
 
-	const onSubmit = (data) => { 
-		console.log(data)
-		history.push("/thanks");
-	};
+class Form extends Component {
+	state = {
+		orderForm: {
+			firstName: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter your First name'
+				},
+				value: ''
+			},
+			lastName: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter your Last name'
+				},
+				value: ''
+			},
+			dateOfBirth: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'date',
+					placeholder: 'YYYY-MM-DD'
+				},
+				value: ''
+			},
+			phoneNum: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'date',
+					placeholder: 'YYYY-MM-DD'
+				},
+				value: ''
+			},
+			
+			country: 'Germany',
+			email: 'test@test.com',
+			deliveryMethod: 'fastest'
+		}
+	}
 
-	const [startDate, setStartDate] = useState();
+	orderHandler = (event) => {
+		event.preventDefault();
+		this.setState({ loading: true });
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.price,
+		}
+	}
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<label htmlFor="firstName">First Name
-				<input type="text" name="firstName" className="input-box" id="firstName" placeholder="Enter your First name"
-					{...register("firstName", { required: true, pattern: /^[A-Z]+|[a-z]+$/, })}
-				/>
-			</label>
+	render() {
+		let form = (
+			<form>
+				<section class="cell large-2">
+					<label for="salutation">Prefix
+                    <select name="salutation" id="salutation">
+							<option value="Mr">Mr</option>
+							<option value="Mrs">Mrs</option>
+							<option value="Miss">Miss</option>
+						</select>
+					</label>
+				</section>t
+				<Input inputtype="input" type="text" name="firstName" placeholder="Ener your First name" />
+				<Input inputtype="input" type="text" name="lastName" placeholder="Enter your Last name" />
+				<Input inputtype="input" type="date" name="dateOfBirth" placeholder="YYYY-MM-DD" />
+				<Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+				<Input inputtype="input" type="text" name="street" placeholder="Street" />
+				<Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+				<button className="button" id="formSubmit" clicked={this.orderHandler}>ORDER</button>
+			</form>
+		);
 
-			<label htmlFor="lastName">Last Name
-				<input type="text" name="lastName" className="input-box" id="lastName" placeholder="Enter your Last name"
-					{...register("lastName", { required: true, pattern: /^[A-Z]+|[a-z]+$/, })}
-				/>
-			</label>
-
-			<label htmlFor="dateOfBirth">Date of Birth</label>
-				<DatePicker name="dateOfBirth"
-					selected={startDate}
-					onChange={date => setStartDate(date)}
-					maxDate={addDays(new Date(), -5844)}
-					dateFormat="yyyy/MM/dd"
-					placeholderText="YYYY/MM/DD"
-					isClearable
-					showYearDropdown
-					showMonthDropdown
-					scrollableMonthYearDropdown
-					dropdownMode="select"
-				/>
-
-
-
-			<input type="submit" />
-		</form>
-	);
+		return (
+			<div>
+				<h4>Enter your Contact Data</h4>
+				{form}
+			</div>
+		);
+	}
 }
 
 export default Form;
