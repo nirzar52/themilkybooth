a79c0be1ec723174bf6cfe2281f4f47d62ff3a74
Nirzar <nirzarsnowbell@gmail.com>
2021-04-08 11:06:37 -0400

    created Form

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,22 +5,29 @@ export const Input = (props) => {
 
     let inputElement = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className="input-box" {...props} />;
+            inputElement = <input className="input-box" {...props.elementConfig} value={props.value} required />;
             break;
         case ('textarea'):
-            inputElement = <textarea className="input-box" {...props} />;
+            inputElement = <textarea className="input-box" {...props.elementConfig} value={props.value} required />;
+            break;
+        case ('select'):
+            inputElement = (<select className="input-box" value={props.value} required>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>{option.displayValue}</option>
+                ))}
+            </select>);
             break;
         default:
-            inputElement = <input className="input-box" {...props} />;
+            inputElement = <input className="input-box" {...props.elementConfig} value={props.value} required />;
 
     }
 
     return (
         <section className="cell large-4">
-        <label>{props.label}</label>
-        { inputElement }
+            <label>{props.label}</label>
+            { inputElement}
         </section>
     )
 }
