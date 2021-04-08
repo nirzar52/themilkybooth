71a10dda383e858a582ca138cec0926262817d6f
Nirzar <nirzarsnowbell@gmail.com>
2021-04-08 11:06:37 -0400

    created Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,17 @@ import Input from './Input';
 class Form extends Component {
 	state = {
 		orderForm: {
+			salutation: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'Mr', displayValue: 'Mr' },
+						{ value: 'Mrs', displayValue: 'Mrs' },
+						{ value: 'Miss', displayValue: 'Miss' },
+					]
+				},
+				value: ''
+			},
 			firstName: {
 				elementType: 'input',
 				elementConfig: {
@@ -29,48 +40,137 @@ class Form extends Component {
 				},
 				value: ''
 			},
-			phoneNum: {
+			emailId: {
 				elementType: 'input',
 				elementConfig: {
-					type: 'date',
-					placeholder: 'YYYY-MM-DD'
+					type: 'email',
+					placeholder: 'Enter email'
 				},
 				value: ''
 			},
-			
+			phoneNum: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter phone number'
+				},
+				value: ''
+			},
+			homeAddress1: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter address line 1'
+				},
+				value: ''
+			},
+			homeAddress2: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter address line 2 (optional)'
+				},
+				value: ''
+			},
+			city: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter city name'
+				},
+				value: ''
+			},
+			postalCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Enter ZIP/Postal code'
+				},
+				value: ''
+			},
+			province: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'Alberta', displayValue: 'AB' },
+						{ value: 'British Colombia', displayValue: 'BC' },
+						{ value: 'Manitoba', displayValue: 'MB' },
+						{ value: 'New Brunswick', displayValue: 'NB' },
+						{ value: 'Newfoundland and Labrador', displayValue: 'NL' },
+						{ value: 'Northwest Territories', displayValue: 'NT' },
+						{ value: 'Nova Scotia', displayValue: 'NS' },
+						{ value: 'Nonavut', displayValue: 'NU' },
+						{ value: 'Ontario', displayValue: 'ON' },
+						{ value: 'Prince Edward Island', displayValue: 'PE' },
+						{ value: 'Quebec', displayValue: 'QC' },
+						{ value: 'Saskatchewan', displayValue: 'SK' },
+						{ value: 'Yukon', displayValue: 'YT' },
+					]
+				},
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					name: 'country',
+					placeholder: 'Enter country',
+					disabled: 'disabled',
+				},
+				value: 'Canada',
+			},
+			termsAndConditions: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'checkbox',
+				},
+			},
+			newsletterSignup: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'checkbox',
+				},
+			},
+
+
 			country: 'Germany',
 			email: 'test@test.com',
 			deliveryMethod: 'fastest'
 		}
 	}
 
-	orderHandler = (event) => {
-		event.preventDefault();
-		this.setState({ loading: true });
-		const order = {
-			ingredients: this.props.ingredients,
-			price: this.props.price,
-		}
-	}
+	orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+    }
 
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			})
+		}
+
 		let form = (
 			<form>
-				<section class="cell large-2">
-					<label for="salutation">Prefix
-                    <select name="salutation" id="salutation">
-							<option value="Mr">Mr</option>
-							<option value="Mrs">Mrs</option>
-							<option value="Miss">Miss</option>
-						</select>
-					</label>
-				</section>t
-				<Input inputtype="input" type="text" name="firstName" placeholder="Ener your First name" />
-				<Input inputtype="input" type="text" name="lastName" placeholder="Enter your Last name" />
-				<Input inputtype="input" type="date" name="dateOfBirth" placeholder="YYYY-MM-DD" />
-				<Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-				<Input inputtype="input" type="text" name="street" placeholder="Street" />
-				<Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+				{formElementsArray.map(formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+					/>
+				))}
 				<button className="button" id="formSubmit" clicked={this.orderHandler}>ORDER</button>
 			</form>
 		);
