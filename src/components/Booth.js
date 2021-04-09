27958d53af4425f3fc567ddfbbc0befe94804a31
Nirzar <nirzarsnowbell@gmail.com>
2021-04-09 19:10:18 -0400

    Photobooth designed

--- src/components/Booth.js
+++ src/components/Booth.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import captureImg from "../images/capture.png";
+import stickerImg from "../images/sticker.png";
+import filterImg from "../images/filter.png";
 
 class Booth extends Component {
     state = {
@@ -26,7 +29,6 @@ class Booth extends Component {
         }
     }
 
-
     takeSelfie = async () => {
         // Get the exact size of the video element.
         const width = this.videoEle.current.videoWidth;
@@ -51,29 +53,6 @@ class Booth extends Component {
         })
     }
 
-    putFilter = async () => {
-        // Get the exact size of the video element.
-        const width = this.videoEle.current.videoWidth;
-        const height = this.videoEle.current.videoHeight;
-
-        // get the context object of hidden canvas
-        const ctx = this.canvasEle.current.getContext('2d');
-
-        // Set the canvas to the same dimensions as the video.
-        this.canvasEle.current.width = width;
-        this.canvasEle.current.height = height;
-
-        // Draw the current frame from the video on the canvas.
-        ctx.drawImage(this.videoEle.current, 0, 0, width, height);
-
-        // Get an image dataURL from the canvas.
-        const imageDataURL = this.canvasEle.current.toDataURL('image/png');
-        this.stopCam();
-
-        this.setState({
-            imageURL: imageDataURL
-        })
-    }
 
     stopCam = () => {
         const stream = this.videoEle.current.srcObject;
@@ -95,40 +74,39 @@ class Booth extends Component {
 
 
     render() {
-        return (<div className="selfie">
-            {this.state.imageURL === '' && <div className="cam">
-                <video width="100%" height="100%" className="video-player" autoPlay={true} ref={this.videoEle}></video>
-                <button className="btn capture-btn" onClick={this.putSticket}>
-                    
-                </button>
-                <button className="btn capture-btn" onClick={this.takeSelfie}>
-                    
-                </button>
-                <button className="btn capture-btn" onClick={this.putFilter}>
-                    
-                </button>
-            </div>
-            }
+        return (
+            <div className="selfie">
+                <div className="frame">
 
+                    {this.state.imageURL === '' && <div className="cam">
+                        <video width="100%" height="100%" className="video-player" autoPlay={true} ref={this.videoEle}></video>
+                        <div className="buttons-container align-middle grid-x align-spaced">
+                            <div className="column text-center small-4"><img src={stickerImg} onClick={this.putStickers} alt="STICKERS" /></div>
+                            <div className="column text-center small-4"><img src={captureImg} onClick={this.takeSelfie} alt="TAKE" /></div>
+                            <div className="column text-center small-4"><img src={filterImg} onClick={this.putFilters} alt="FILTERS" /></div>
+                        </div>
+                    </div>
+                    }
 
-            <canvas ref={this.canvasEle} style={{ display: 'none' }}></canvas>
-            {this.state.imageURL !== '' && <div className="preview">
-                <img className="preview-img" src={this.state.imageURL} ref={this.imageEle} />
+                    <canvas ref={this.canvasEle} style={{ display: 'none' }}></canvas>
+                    {this.state.imageURL !== '' && <div className="preview">
+                        <img className="preview-img" src={this.state.imageURL} ref={this.imageEle} />
 
-                <div className="btn-container">
-                    <button className="btn back-btn" onClick={this.backToCam}>
-                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                    </button>
-                    <a href={this.state.imageURL} download="selfie.png"
-                        className="btn download-btn">
-                        <i class="fa fa-download" aria-hidden="true"></i>
-                    </a>
+                        <div className="btn-container">
+                            <button className="btn back-btn" onClick={this.backToCam}>
+                                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                            </button>
+                            <a href={this.state.imageURL} download="selfie.png"
+                                className="btn download-btn">
+                                <i class="fa fa-download" aria-hidden="true"></i>
+                            </a>
+                        </div>
+
+                    </div>
+                    }
                 </div>
-
             </div>
-            }
-
-        </div>)
+        )
     }
 }
 
