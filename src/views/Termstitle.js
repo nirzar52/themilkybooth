aa1399fc7e09146c4b46ec23957dc1d29c0dce88
Nirzar <nirzarsnowbell@gmail.com>
2021-04-09 02:54:00 -0400

    accordion added

--- src/views/Termstitle.js
+++ src/views/Termstitle.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Termstitle() {
-    return(
-        <section className="callout term-title page-title">
-                <section className="grid-container">
-                    <h1 className="text-center small-text-left">Term and conditions</h1>
-                </section>
+    return (
+        <section className="blue-callout term-title page-title">
+            <section className="grid-container">
+                <h1 className="oswald text-center small-text-left">Term and conditions</h1>
+            </section>
         </section>
     )
 }
